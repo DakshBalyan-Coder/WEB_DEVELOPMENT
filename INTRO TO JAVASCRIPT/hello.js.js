4d300c5d1631d7ecdb61adbae3bfb75b4de40110
Daksh Balyan <130841662+DakshBalyan-Coder@users.noreply.github.com>
2023-04-24 07:30:32 +0530

    Add files via upload

---
+++ INTRO TO JAVASCRIPT/hello.js.js
@@ -0,0 +1,5 @@
+var tweet = prompt("Compose your tweet:");
+var tweetCount = tweet.length;
+alert("You have written" + tweetCount + " characters, you have " + (140 - tweetCount) + " characters remaining.");
+var name = "Daksh";
+name.slice(1,3);
