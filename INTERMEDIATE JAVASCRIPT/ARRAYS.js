b4510610983550f9fadc68f49319005b76ddb94d
Daksh Balyan <130841662+DakshBalyan-Coder@users.noreply.github.com>
2023-05-26 21:49:29 +0530

    Add files via upload

---
+++ INTERMEDIATE JAVASCRIPT/ARRAYS.js
@@ -0,0 +1,6 @@
+var guestname = ["Yash","Eshita","Mridul","Kashvi","Tanish","Neha"];
+console.log(guestname);
+console.log(guestname.length);
+console.log(guestname[3]);
+var guestlist = prompt("what is your name?");
+guestname.includes(guestlist);
