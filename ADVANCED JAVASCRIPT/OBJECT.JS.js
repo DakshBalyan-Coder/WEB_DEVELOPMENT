069fd3b83ee04f96b8d5cc1f034c71deab2c3bbf
Daksh Balyan <130841662+DakshBalyan-Coder@users.noreply.github.com>
2023-05-28 22:04:28 +0530

    Add files via upload

---
+++ ADVANCED JAVASCRIPT/OBJECT.JS.js
@@ -0,0 +1,9 @@
+
+function HouseKeeper(yearsOfExperience,name,cleaningReportire){
+    this.yearsOfExperience = yearsOfExperience;
+    this.name = name;
+    this.cleaningReportire = cleaningReportire;
+    this.clean= function(){
+        alert("Cleaning in Progress....")
+ }
+}
